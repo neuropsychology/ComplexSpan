2709787ba6056ee9f84ad52cbed417af1b9c10ed
Zen <lauzenjuen@gmail.com>
2021-05-21 10:00:38 +0800

    option to skip complex span computation (if no CS trials were answered correctly)

--- statistics/preprocessing.R
+++ statistics/preprocessing.R
@@ -5,13 +5,27 @@
 #
 # Example
 # path <- "./data/pilot_2.csv"
-preprocess_WM <- function(path){
-  cleaned <- .clean_data(read.csv(path))
+preprocess_WM <- function(path, skip_complexspan=""){  
+  
+  # Clean data
+  cleaned <- .clean_data(read.csv(path), skip_complexspan=skip_complexspan)
+  
+  # Compute Simple Span
   SimpleSpan <- .clean_SimpleSpan(cleaned)
-  ComplexSpan <- .clean_ComplexSpan(cleaned)
-
-  SimpleSpan[names(ComplexSpan)[!names(ComplexSpan) %in% names(SimpleSpan)]] <- NA
-  out <- rbind(SimpleSpan, ComplexSpan)
+  
+  # Compute Complex Span
+  if (skip_complexspan != cleaned$Participant[1]){
+    ComplexSpan <- .clean_ComplexSpan(cleaned)
+    SimpleSpan[names(ComplexSpan)[!names(ComplexSpan) %in% names(SimpleSpan)]] <- NA
+    out <- rbind(SimpleSpan, ComplexSpan)
+  } else {
+    out <- SimpleSpan %>% 
+      add_column(Distractor = NA,
+                 Distractor_Correct = NA,
+                 Distractor_RT = NA)
+    paste0("Skipping computation of complex span for ", skip_complexspan, ".")
+  }
+  
   out <- dplyr::arrange(out, desc(Task), Trial, Correct)
   row.names(out) <- NULL
   out[c("Participant", "Task", "Set_Size", "Trial", "Stimulus", "Response", "Correct", "Distractor", "Distractor_Correct", "Distractor_RT")]
@@ -19,12 +33,36 @@ preprocess_WM <- function(path){
 
 # Utility functions -------------------------------------------------------
 
+# # path <- data[[43]]
+# data <- read.csv(path)
 
-
-.clean_data <- function(data){
-  data <- dplyr::select(data, Participant = participant, Problem, Operation_answer, digit_answer, complex_recall_corr, complex_recall_rt = key_resp_8.rt, complex_proc_corr = key_resp_7.corr, complex_proc_rt = key_resp_7.rt, complex_prac_corr, key_resp_9.rt, key_resp_11.corr, key_resp_11.rt, key_resp.corr, key_resp.rt, processing_score, simple_recall_corr, simple_recall_rt = key_resp_5.rt, simple_recall, complex_recall, simple_recall_corrkeys, complex_answer = complex_recall_corrkeys)
-
-
+.clean_data <- function(data, skip_complexspan=""){
+  
+  if (data$participant[1] == skip_complexspan) {
+    
+    data <- dplyr::select(data, Participant = participant, Problem, Operation_answer, digit_answer,
+                          key_resp_11.corr, key_resp_11.rt, key_resp.corr,
+                          key_resp.rt, processing_score, simple_recall_corr, simple_recall_rt = key_resp_5.rt,
+                          simple_recall, simple_recall_corrkeys) %>%
+      add_column(complex_recall_corr = NA,
+                 complex_recall_rt = NA,
+                 complex_proc_corr = NA,
+                 complex_proc_rt = NA,
+                 complex_prac_corr = NA,
+                 key_resp_9.rt = NA,
+                 complex_recall = NA,
+                 complex_answer = NA
+      ) %>% 
+      select(Participant, Problem, Operation_answer, digit_answer, complex_recall_corr,
+             complex_recall_rt, complex_proc_corr, complex_proc_rt, complex_prac_corr, key_resp_9.rt,
+             key_resp_11.corr, key_resp_11.rt, key_resp.corr, key_resp.rt, processing_score,
+             simple_recall_corr, simple_recall_rt, simple_recall, complex_recall,
+             simple_recall_corrkeys, complex_answer)
+    
+  } else {
+    data <- dplyr::select(data, Participant = participant, Problem, Operation_answer, digit_answer, complex_recall_corr, complex_recall_rt = key_resp_8.rt, complex_proc_corr = key_resp_7.corr, complex_proc_rt = key_resp_7.rt, complex_prac_corr, key_resp_9.rt, key_resp_11.corr, key_resp_11.rt, key_resp.corr, key_resp.rt, processing_score, simple_recall_corr, simple_recall_rt = key_resp_5.rt, simple_recall, complex_recall, simple_recall_corrkeys, complex_answer = complex_recall_corrkeys)
+  }
+  
   data$simple_setsize <- nchar(as.character(data$simple_recall_corrkeys))
   data$complex_setsize <- nchar(as.character(data$complex_answer))
   data$simple_recall_corrkeys <- as.character(data$simple_recall_corrkeys)
@@ -32,7 +70,7 @@ preprocess_WM <- function(path){
   data$complex_answer <- as.character(data$complex_answer)
   data$complex_recall <- as.character(data$complex_recall)
   data$Participant <- as.character(data$Participant)
-
+  
   data <- dplyr::mutate_if(data, is.character, stringr::str_to_upper)
   data
 }
@@ -40,7 +78,7 @@ preprocess_WM <- function(path){
 .clean_SimpleSpan <- function(data){
   Simple_Span <- dplyr::select(data, Participant, Stimulus = simple_recall_corrkeys, Response = simple_recall, Correct = simple_recall_corr, Set_Size = simple_setsize)
   Simple_Span <- Simple_Span[!is.na(Simple_Span$Correct), ]
-
+  
   Simple_Span <- dplyr::mutate(Simple_Span,
                                Task = "Simple",
                                Trial = 1:nrow(Simple_Span)
@@ -55,31 +93,37 @@ preprocess_WM <- function(path){
 
 
 .clean_ComplexSpan <- function(data){
-
+  
   Complex_Span <- dplyr::select(data, Participant, Distractor = Problem, Operation_answer, digit_answer, Correct = complex_recall_corr, Response = complex_recall, Distractor_Correct = complex_proc_corr, Distractor_RT = complex_proc_rt, Stimulus = complex_answer, Set_Size = complex_setsize)
-
-
+  
   Complex_Span <- Complex_Span[(!is.na(Complex_Span$Distractor_RT) & Complex_Span$Distractor_RT != "")| (!is.na(Complex_Span$Response) & Complex_Span$Response != ""), ] # remove irrelevant NA rows
-
+  
   Complex_Span$Distractor_RT <- stringr::str_remove(Complex_Span$Distractor_RT, "\\]")
   Complex_Span$Distractor_RT <- stringr::str_remove(Complex_Span$Distractor_RT, "\\[")
   Complex_Span$Distractor_RT <- as.numeric(Complex_Span$Distractor_RT)
-
+  
   for(var in c("Stimulus", "Response", "Distractor_RT", "Distractor")){
     Complex_Span[[var]] <- as.character(Complex_Span[[var]])
     Complex_Span[[var]] <- ifelse(Complex_Span[[var]] == "", NA, Complex_Span[[var]])
   }
   Complex_Span$Set_Size <- ifelse(Complex_Span$Set_Size == 0, NA, Complex_Span$Set_Size)
-
+  
   Complex_Span <- tidyr::fill(Complex_Span, Correct, Response, Stimulus, Set_Size, .direction = c("up"))
   Complex_Span <- Complex_Span[!is.na(Complex_Span$Distractor_RT),]
   Complex_Span$Distractor_Correct = ifelse(Complex_Span$Operation_answer == Complex_Span$digit_answer, 1, 0)
-
+  
   Complex_Span$Distractor_RT <- as.numeric(Complex_Span$Distractor_RT)
   Complex_Span$Task <- "Complex"
   Complex_Span$Operation_answer <- NULL
   Complex_Span$digit_answer <- NULL
-
+  
+  if (sum(is.na(Complex_Span$Stimulus)) > 0) {
+    n_removed <- sum(is.na(Complex_Span$Stimulus))
+    subject <- unique(Complex_Span$Participant)
+    Complex_Span <- Complex_Span[complete.cases(Complex_Span), ]
+    paste0("Removing ", n_removed, " trials with no final answer for ", subject, ", likely due to experiment crashing at the end.")
+  }
+  
   N <- 1:length(unique(Complex_Span$Stimulus)) # append trial number
   Duplicates <- as.data.frame(table(Complex_Span$Stimulus))
   Complex_Span$Trial <- rep(N, sort(Duplicates$Freq))
--- statistics/processing.R
+++ statistics/processing.R
@@ -7,17 +7,17 @@
 # Example
 # path <- "./data/pilot_2.csv"
 
-process_WM <- function(path){
-  data <- preprocess_WM(path)
-
+process_WM <- function(path, skip_complexspan=""){
+  data <- preprocess_WM(path, skip_complexspan=skip_complexspan)
+  
   # Split task type
   Simple_Task <- dplyr::filter(data, Task == "Simple")
   Complex_Task <- dplyr::filter(data, Task == "Complex")
-
+  
   # Adjusted Scores
-  Adjust_SS <- sum(Simple_Task$Correct[which(Simple_Task$Set_Size == max(Simple_Task$Set_Size))])/length(Simple_Task$Correct[which(Simple_Task$Set_Size == max(Simple_Task$Set_Size))])
+  Adjust_SS <- sum(Simple_Task$Correct[which(Simple_Task$Set_Size == max(Simple_Task$Set_Size))])/length(Simple_Task$Correct[which(Simple_Task$Set_Size == max(Simple_Task$Set_Size))]) # proportion of correct recalls at last set size
   Adjust_CS <- sum(Complex_Task$Correct[which(Complex_Task$Set_Size == max(Complex_Task$Set_Size))])/length(Complex_Task$Correct[which(Complex_Task$Set_Size == max(Complex_Task$Set_Size))])
-
+  
   ### Table of Scores
   # Span Scores per Participant
   Between_Participant <- dplyr::mutate(data,
@@ -28,8 +28,8 @@ process_WM <- function(path){
                                        RT_Mean = mean(Distractor_RT, na.rm = TRUE)) %>%
     dplyr::select(Participant, Span_Simple, Span_Simple_Adjusted, Span_Complex, Span_Complex_Adjusted, RT_Mean) %>%
     unique()
-
-
+  
+  
   # Change in proportion of Correct Recall, Correct Processing, and Mean RT as set size increases
   Within_Participant <- data %>%
     dplyr::group_by(Participant, Task, Set_Size) %>%
@@ -38,18 +38,18 @@ process_WM <- function(path){
       RT_Mean = mean(Distractor_RT, na.rm = TRUE),
       Distractor_Correct = sum(Distractor_Correct, na.rm = TRUE)/length(Distractor_Correct)) %>%
     dplyr::ungroup()
-
+  
   Within_Participant$Distractor_Correct <- ifelse(Within_Participant$RT_Mean == "NaN", "NaN", Within_Participant$Distractor_Correct)
   Within_Participant$Distractor_Correct <- as.numeric(Within_Participant$Distractor_Correct)
-
-
+  
+  
   # Mean RT and Proportion Correct per Distractor
   Distractors <- Complex_Task %>%
     dplyr::group_by(Participant, Distractor) %>%
     dplyr::summarize(RT_Mean = mean(Distractor_RT, na.rm = TRUE),
                      Distractor_Correct = sum(Distractor_Correct, na.rm = TRUE)/length(Distractor_Correct)) %>%
     dplyr::ungroup()
-
+  
   return(list(between = Between_Participant, within = Within_Participant, distractors = Distractors))
-  }
+}
 
